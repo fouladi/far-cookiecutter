f2da7c6fd5b33da27e30ed48240adb272542bf8c
Farhad Fouladi <fouladi@gmx.de>
2024-08-16 07:53:53 +0200

    feat: update requirements for development

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/util.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/util.py
@@ -1,4 +1,5 @@
 """Utilities for logging and parameters setup"""
+
 import logging
 from argparse import ArgumentParser, RawTextHelpFormatter
 from logging.handlers import RotatingFileHandler
@@ -18,13 +19,11 @@ def log_setup(fname, logger):
     filename = "log/" + fname
     _log = logging.getLogger(logger)
     _log.setLevel(logging.INFO)
-    handler = RotatingFileHandler(filename,
-                                  mode='a',
-                                  maxBytes=50 * 1024 * 1024,
-                                  backupCount=10)
+    handler = RotatingFileHandler(filename, mode="a", maxBytes=50 * 1024 * 1024, backupCount=10)
     formatter = logging.Formatter(
-        '%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s [%(module)s:%(funcName)s:%(lineno)d] %(threadName)s',
-        '%Y-%m-%d %H:%M:%S')
+        "%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s [%(module)s:%(funcName)s:%(lineno)d] %(threadName)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
     handler.setFormatter(formatter)
     _log.addHandler(handler)
     return _log
@@ -33,22 +32,21 @@ def log_setup(fname, logger):
 def arg_setup():
     """Parse all input arguments."""
 
-    pars = ArgumentParser('{{cookiecutter.repo_name}}.py',
-                          usage='%(prog)s  {version} [options]',
-                          formatter_class=RawTextHelpFormatter,
-                          description="""
+    pars = ArgumentParser(
+        "{{cookiecutter.repo_name}}.py",
+        usage="%(prog)s  {version} [options]",
+        formatter_class=RawTextHelpFormatter,
+        description="""
         Usage Examples:
         ---------------
             run_{{cookiecutter.repo_name}}.sh fib --number 5
-            """)
-    subparsers = pars.add_subparsers(dest='command')
+            """,
+    )
+    subparsers = pars.add_subparsers(dest="command")
 
-    _ = subparsers.add_parser('version', help='Version')
-    fib_parser = subparsers.add_parser('fib', help='Version')
-    fib_parser.add_argument('-n',
-                            '--number',
-                            help='Calculate Fibonnaci Number',
-                            required=True)
+    _ = subparsers.add_parser("version", help="Version")
+    fib_parser = subparsers.add_parser("fib", help="Version")
+    fib_parser.add_argument("-n", "--number", help="Calculate Fibonnaci Number", required=True)
     return pars.parse_args()
 
 
